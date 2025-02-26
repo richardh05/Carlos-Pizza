1ce2966365bd275579600eeb5fcf2996c0403b9b
Richard <57541025+richardh05@users.noreply.github.com>
2025-02-26 18:03:45 +0000

    Admin page shows unavailable items; Menu and Home only show available items; Home shows menu images

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Carlos_Pizza.Data;
+using Carlos_Pizza.Models;
 
 namespace Carlos_Pizza.Pages;
 
 [Authorize (Roles = "Admin")]
 public class Admin : PageModel
 {
+    private readonly ILogger<Admin> _logger;
+
+    public Admin(ILogger<Admin> logger, CarlosDB context)
+    {
+        _logger = logger;
+        _db = context;
+    }
+    
+    public IList<MenuItem> MenuItems { get; set; } = default;
+        
+    private readonly CarlosDB _db;
     public void OnGet()
     {
-        
+        MenuItems = _db.MenuItems.FromSqlRaw("SELECT * FROM MenuItem").ToList();
     }
 }
