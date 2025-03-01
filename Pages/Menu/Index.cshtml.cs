b0e3b114edb7d20ee1011dac55515bbaa591b4ab
Richard <57541025+richardh05@users.noreply.github.com>
2025-03-01 16:17:46 +0000

    Menu index now has basket support!

--- Pages/Menu/Index.cshtml.cs
+++ Pages/Menu/Index.cshtml.cs
@@ -7,23 +7,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Carlos_Pizza.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Carlos_Pizza.Pages.Menu
 {
-    public class IndexModel : PageModel
+    public class IndexModel(CarlosDB context, UserManager<IdentityUser> userManager) : PageModel
     {
-        private readonly CarlosDB _context;
-
-        public IndexModel(CarlosDB context)
-        {
-            _context = context;
-        }
-
         public IList<MenuItem> MenuItem { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            MenuItem = await _context.MenuItems.ToListAsync();
+            MenuItem = await context.MenuItems.ToListAsync();
         }
         // === Searchbar ===
         [BindProperty]
@@ -34,18 +28,52 @@ namespace Carlos_Pizza.Pages.Menu
             {
                 // Use LINQ to filter menu items based on the search string
                 // Using SQL queries can be insecure, so I'm using the objects here
-                MenuItem = _context.MenuItems
+                MenuItem = context.MenuItems
                     .Where(m => m.Name.Contains(SearchString))
                     .ToList();
             }
             else
             {
                 // If no search string, return all items
-                MenuItem = _context.MenuItems.ToList();
+                MenuItem = context.MenuItems.ToList();
             }
 
             return Page();
 
+        }
+
+        public async Task<IActionResult> OnPostBuyAsync(int itemId)
+        {
+            var user = await userManager.GetUserAsync(User);
+            CheckoutCustomer customer = await context.CheckoutCustomers.FindAsync(user.Email);
+            
+            var item = context.BasketItems.FromSqlRaw("SELECT * FROM BasketItems WHERE StockID = {0}" + " AND BasketID = {1}", itemId, customer.BasketId).ToList().FirstOrDefault();
+
+            if (item == null)
+            {
+                BasketItem newItem = new BasketItem
+                {
+                    BasketId = customer.BasketId,
+                    StockId = itemId,
+                    Quantity = 1
+                };
+                context.BasketItems.Add(newItem);
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                item.Quantity++;
+                context.Attach(item).State = EntityState.Modified;
+                try
+                {
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    throw new Exception($"Basket not found!", e);
+                }
+            }
+            return RedirectToPage();
         }
         }
 }
